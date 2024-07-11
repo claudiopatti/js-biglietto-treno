0b47207a7f9071822610edf33b202c9e8b612ea6
claudiopatti <claudiopatti1997@gmail.com>
2024-07-11 23:54:34 +0200

    aggiunto layout e caratteristeche del bottone interattivo

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 console.log('ciao console');
 
-const kilometers = prompt("Quanti chilometri dovrai percorrere?");
+const kilometers = "";
+
 console.log('chilometri da percorrere:' + kilometers);
 
-const age = prompt('Quanti anni hai?');
+
+const age = "";
+
 console.log('Età:' + age);
 
-const priceTicket = kilometers * 0.21 ;
+const priceTicket = (Math.round((kilometers * 0.21) * 100) / 100) ;
 console.log('prezzo del biglietto:' + priceTicket + '€');
 
 const priceTicketForMinor = (Math.round((kilometers * 0.168) * 100) / 100);
@@ -21,16 +24,19 @@ console.log('prezzo del biglietto per anziani:' + priceTicketForOver.toFixed(2) 
 const ageMinor = 17;
 const ageOver = 65;
 
-if (age <= ageMinor ) {
-    console.log('prezzo scontato per minori:' + priceTicketForMinor + '€' );
-}
-else if (age >= ageOver) {
-    console.log('prezzo scontato per anziani:' + priceTicketForOver.toFixed(2) + '€' );
-}
+// if (age <= ageMinor ) {
+//     document.getElementById('resultPrice').innerHTML = priceTicketForMinor + '€'
+//     console.log('prezzo scontato per minori:' + priceTicketForMinor + '€' );
+// }
+// else if (age >= ageOver) {
+//     document.getElementById('resultPrice').innerHTML = priceTicketForOver + '€'
+//     console.log('prezzo scontato per anziani:' + priceTicketForOver.toFixed(2) + '€' );
+// }
 
-else {
-    console.log('prezzo normale' + priceTicket + '€');
-}
+// else {
+//     document.getElementById('resultPrice').innerHTML = priceTicket + '€'
+//     console.log('prezzo normale' + priceTicket + '€');
+// }
 
 // if (age >= ageOver) {
 //     console.log('prezzo scontato per anziani:' + priceTicketForOver + '€' )
@@ -38,3 +44,50 @@ else {
 // else {
 //     console.log('prezzo normale' + priceTicket + '€')
 // }
+
+
+// document.getElementById('resultPrice').innerHTML = 
+
+function myFuction(kilometers, age) {
+
+    console.log(kilometers.value, age.value);
+    kilometers = (kilometers.value)
+    age = (age.value)
+    
+
+    console.log('chilometri da percorrere:' + kilometers);
+    
+    console.log('Età:' + age);
+
+    const priceTicket = (Math.round((kilometers * 0.21) * 100) / 100);
+    console.log('prezzo del biglietto:' + priceTicket + '€');
+
+    const priceTicketForMinor = (Math.round((kilometers * 0.168) * 100) / 100);
+    console.log('prezzo del biglietto per minorenni:' + priceTicketForMinor + '€');
+
+
+    const priceTicketForOver = kilometers * 0.126;
+    console.log('prezzo del biglietto per anziani:' + priceTicketForOver.toFixed(2) + '€');
+
+    const ageMinor = 17;
+    const ageOver = 65;
+
+    if (age <= ageMinor) {
+       
+        document.getElementById('resultPrice').innerHTML = priceTicketForMinor + '€'
+        console.log('prezzo scontato per minori:' + priceTicketForMinor + '€');
+    }
+    else if (age >= ageOver) {
+        document.getElementById('resultPrice').innerHTML = priceTicketForOver + '€'
+        console.log('prezzo scontato per anziani:' + priceTicketForOver.toFixed(2) + '€');
+    }
+
+    else {
+        document.getElementById('resultPrice').innerHTML = priceTicket + '€'
+        console.log('prezzo normale:' + priceTicket + '€');
+
+    }
+
+
+}
+
